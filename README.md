c11cf11bb95b48f77ee0aa927dc2a8cf55dee504
Alex Ng <alextemp68@gmail.com>
2025-05-30 00:41:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Open dist/game.html to play.
+2. To edit, edit files in src/ and also run `tsc -w`. It'll build the ts file
+   and write it into dist/.
---
+++ src/game.ts
@@ -0,0 +1,391 @@
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+type Planet = {
+  name: string;
+  pos: number;
+  goods: number;
+  display?: string;
+  launchCost: number;
+}
+
+type Ship = {
+   destination1: Planet;
+   destination2: Planet;
+   name: string;
+   pos: number;
+   direction: boolean; // True means going to destination 2
+   speed: number;
+   capacity: number;
+   upgradeSpeedCost: number;
+   upgradeCapacityCost: number;
+}
+
+const planetA: Planet = {name: "A", pos: 0, goods: 1, launchCost: 0};
+const planetB: Planet = {name: "B", pos: 8, goods: 1, launchCost: 9,
+  display: `         ,MMM8&&&.
+    _...MMMMM88&&&&..._
+ .::'''MMMMM88&&&&&&'''::.
+::     MMMMM88&&&&&&     ::
+'::....MMMMM88&&&&&&....::'
+   '''''MMMMM88&&&&'''''
+         'MMM8&&&'
+`};
+const planetC: Planet = {name: "C", pos: 15, goods: 2, launchCost: 15,
+  display: ` ~+       *       +
+    '                  |
+()    .-.,="\`\`"=.    - o -
+      '=/_       \     |
+   *   |  '=._    |
+        \     \`=./\`,        '
+     .   '=.__.=' \`='      *
+                      +
+ O      *        '       .
+`};
+const planetD: Planet = {name: "D", pos: 30, goods: 4, launchCost: 25,
+  display: `⢞⡵⣫⢞⡵⣫⢞⣵⣫⣞⡵⠛⠚⠓⠛⢺⣵⣫⢞⡵⣫⢞⡵⣫⢞⡵
+⣮⢳⡝⣮⢳⣝⡾⢊⢡⠂⠐⠁⢩⠴⣵⣦⡤⡉⠙⢾⡱⣏⢾⡱⣏⢾
+⣎⢷⡹⣎⡿⠈⠔⠀⠀⠠⠀⠕⠋⠘⡥⡉⣿⣷⣷⣎⠙⢮⡳⣝⢮⡳
+⡼⣣⢟⡵⢃⠁⠀⠀⠀⠡⠄⣠⢄⡐⢃⠶⣏⡟⣿⣿⣴⡌⣷⡹⣎⢷
+⢞⡵⣫⠙⡌⠀⠀⠀⠀⠠⡀⡄⢀⡘⠥⣘⠐⣲⢸⣿⣿⣇⠊⣷⡹⢮
+⣮⢳⡽⠀⠆⠀⠄⢀⡠⢄⠀⡄⠱⠬⣁⡈⡖⣧⢿⣿⣿⣿⠀⣧⣛⠷
+⣎⢷⣹⣀⠃⠂⠠⠀⠰⡈⢆⢘⡖⠓⢠⢈⡞⣄⣿⣿⣿⡟⣀⠶⣭⣛
+⡼⣣⢷⡹⡜⢤⡋⠑⠠⢱⠀⡏⡄⠢⢥⠜⣼⣿⣿⣿⣿⢡⣏⡟⣶⡹
+⢞⡵⣫⢳⡳⣄⡹⢾⣼⣩⠖⣱⡍⠱⢿⣾⣿⣿⣿⠟⣠⠾⣼⡹⢶⡹
+⣮⢳⣭⢳⡝⣧⢳⡜⣉⠶⠶⢿⠦⡶⢾⠿⠟⣛⢫⢶⣫⢟⡶⣹⢧⡻
+⣎⠷⣎⢷⡹⣎⢷⡹⡽⢶⡳⢤⡤⢤⡤⣴⢻⡞⡽⣎⢷⣫⢞⡵⣫⢷
+`};
+
+const destinationPlanets = [planetB, planetC, planetD];
+
+const upgradeSpeedCost = 5;
+let upgradeCapacityCost = 8;
+
+const ship1: Ship = { name: "Ship 1", destination1: planetA, destination2: planetB, pos: 0, direction: true, speed: 1, capacity: 1, upgradeSpeedCost, upgradeCapacityCost };
+const ship2: Ship = { name: "Ship 2", destination1: planetA, destination2: planetC, pos: 0, direction: true, speed: 1, capacity: 2, upgradeSpeedCost, upgradeCapacityCost };
+const ships = [ship1, ship2];
+
+let credits = 0;
+let gameTick = 0;
+
+const upgradeShipSpeed = (ship: Ship) => {
+  if (credits >= ship.upgradeSpeedCost) {
+    ship.speed += 1;
+    credits -= ship.upgradeSpeedCost;
+
+    switch(ship.speed) {
+      case 2:
+        ship.upgradeSpeedCost += 2;
+        break;
+      case 3:
+        ship.upgradeSpeedCost += 5;
+        break;
+    }
+  }
+}
+
+const upgradeShipCapacity = (ship: Ship) => {
+  if (credits >= ship.upgradeCapacityCost) {
+    ship.capacity += 1;
+    credits -= ship.upgradeCapacityCost;
+    switch(ship.speed) {
+      case 2:
+        ship.upgradeCapacityCost += 3;
+        break;
+      case 3:
+        ship.upgradeCapacityCost += 7;
+        break;
+    }
+  }
+}
+
+const addShipToPlanet = (planet: Planet): Ship | undefined => {
+  if (credits >= planet.launchCost) {
+    const ship: Ship = {
+      name: "Ship " + (ships.length + 1),
+      destination1: planetA,
+      destination2: planet,
+      speed: 1,
+      capacity: 1,
+      pos: 0,
+      direction: true,
+      upgradeSpeedCost,
+      upgradeCapacityCost,
+    }
+
+    credits -= planet.launchCost;
+
+    planet.launchCost = Math.floor(planet.launchCost * 1.5);
+
+    return ship;
+  }
+}
+
+const getOrCreateButton = (
+  { id, textContent, onclick, disabled }:
+  { id: string, textContent?: string, onclick?: () => void, parentDiv?: HTMLElement, disabled?: boolean}
+) => {
+  const newButton = getOrCreateElementById({id, elementTypeArg: "button"}) as HTMLButtonElement;
+  if (textContent) {
+    newButton.textContent = textContent;
+  }
+  if (onclick) {
+    newButton.onclick = onclick;
+  }
+
+  newButton.disabled = !!disabled;
+
+  return newButton;
+};
+
+const getOrCreateElementById = (
+  {id, innerText, elementTypeArg}:
+  {id: string, innerText?: string, elementTypeArg?: string}
+) => {
+    let element = document.getElementById(id);
+    if (!element) {
+      const elementType = elementTypeArg || "div";
+      element = document.createElement(elementType);
+      element.id = id
+    }
+    if (innerText !== undefined) {
+      element.innerText = innerText;
+    }
+    return element;
+};
+
+const debugPauseUntilClick = () => {
+  return new Promise((resolve) => {
+    const button = getOrCreateElementById({
+      id: 'debugNextButton',
+      innerText: "Next",
+      elementTypeArg: "button",
+    });
+    button.addEventListener('click', () => {
+      console.log("click");
+      resolve(undefined);
+    });
+  });
+};
+
+const main = async () => {
+  while (true) {
+    updates();
+    display();
+    // TODO: Only show button etc when in debug mode
+    // TODO: even more sophisticated would be start, stop, next
+    // await debugPauseUntilClick();
+    await sleep(300);
+  }
+}
+
+const updates = () => {
+  ships.forEach((ship) => {
+    if (ship.pos >= ship.destination2.pos) {
+      ship.direction = false;
+      ship.pos == ship.destination2.pos
+    }
+    else if (ship.pos <= ship.destination1.pos) {
+      ship.direction = true;
+      ship.pos == ship.destination1.pos
+      if (gameTick !== 0) {
+        credits += ship.capacity * ship.destination2.goods;
+      }
+    }
+    if (ship.direction) {
+        ship.pos += ship.speed;
+    } else {
+      ship.pos -= ship.speed;
+    }
+  });
+
+  gameTick += 1;
+}
+
+const display = () => {
+  // Ship Display
+  const shipsDiv = getOrCreateElementById({id: "ships"});
+
+  ships.forEach((ship) => {
+    const parentId = "shipParent-" + ship.name;
+    const parentDiv = getOrCreateElementById({id: parentId});
+    const shipName = getOrCreateElementById({id: parentId + "-shipName", innerText: ship.name + ": ", elementTypeArg: "span"});
+    const planet1 = getOrCreateElementById({id: parentId + "-planet1", innerText: ship.destination1.name, elementTypeArg: "span"});
+
+    let shipInnerText = ""
+    if (ship.pos > 0 && ship.pos < ship.destination2.pos) {
+      const showShipAddOnsForwardDir = !(ship.pos === 1 && ship.direction);
+      const showShipAddOnsBackwardDir = !(ship.pos === ship.destination2.pos - 1 && !ship.direction);
+      const showShipAddOns = showShipAddOnsForwardDir && showShipAddOnsBackwardDir;
+
+      if (ship.direction) {
+        shipInnerText += ">"
+      } else {
+        shipInnerText += "<"
+      }
+
+      if (ship.capacity === 2) {
+        shipInnerText += "=";
+      } else if (ship.capacity === 3) {
+        shipInnerText += "==";
+      } else if (ship.capacity > 3) {
+        shipInnerText += "===";
+      }
+
+      if (ship.speed === 2) {
+        shipInnerText += "~"
+      } else if (ship.speed > 2) {
+        const speedDis = ship.direction ? "}" : "{";
+
+        if (ship.speed === 3) {
+          shipInnerText += speedDis;
+        } else if (ship.speed > 3) {
+          shipInnerText += speedDis + speedDis;
+        }
+      }
+
+      let shipLengthCut = 0; Math.min(ship.pos, shipInnerText.length);
+      if (ship.direction) {
+        shipLengthCut = Math.min(ship.pos, shipInnerText.length);
+      } else {
+        shipLengthCut = Math.min(ship.destination2.pos - ship.pos, shipInnerText.length);
+      }
+      shipInnerText = shipInnerText.substring(0, shipLengthCut);
+
+      if (ship.direction) {
+        shipInnerText = shipInnerText.split("").reverse().join("");
+      }
+    }
+    const shipDisplay = getOrCreateElementById({id: parentId + "-shipDisplay", innerText: shipInnerText, elementTypeArg: "span"});
+    const shipLength = shipInnerText.length;
+
+    let preDotsCount = 0;
+    let postDotsCount = 0;
+    if (ship.pos <= 0) {
+      preDotsCount = 0;
+      postDotsCount = ship.destination2.pos - 1 - Math.max(ship.pos, 0);
+    } else if (ship.pos < ship.destination2.pos && ship.direction) {
+      preDotsCount = Math.max(ship.pos - 1 - (shipLength - 1), 0);
+      postDotsCount = ship.destination2.pos - 1 - ship.pos;
+    } else if (ship.pos < ship.destination2.pos && !ship.direction) {
+      preDotsCount = ship.pos - 1;
+      postDotsCount = Math.max(ship.destination2.pos - 1 - ship.pos - (shipLength - 1), 0);
+    } else if (ship.pos >= ship.destination2.pos) {
+      preDotsCount = ship.destination2.pos - 1;
+      postDotsCount = 0;
+    }
+    const preShip = getOrCreateElementById({id: parentId + "-preShip", innerText: ".".repeat(preDotsCount), elementTypeArg: "span"});
+    const postShip = getOrCreateElementById({id: parentId + "-postShip", innerText: ".".repeat(postDotsCount), elementTypeArg: "span"});
+
+
+
+    const planet2 = getOrCreateElementById({id: parentId + "-planet2", innerText: ship.destination2.name, elementTypeArg: "span"});
+
+    parentDiv.appendChild(shipName);
+    parentDiv.appendChild(planet1);
+    parentDiv.appendChild(preShip);
+    parentDiv.appendChild(shipDisplay);
+    parentDiv.appendChild(postShip);
+    parentDiv.appendChild(planet2);
+    shipsDiv.appendChild(parentDiv);
+  });
+
+  // Game info
+  const gameInfoDiv = getOrCreateElementById({id: "gameInfo"});
+
+  let creditsId = "credits";
+  let creditsDiv = getOrCreateElementById({
+    id: "credits",
+    innerText: "Credits: " + credits.toString(),
+  });
+  gameInfoDiv.appendChild(creditsDiv);
+
+  // const gameTickId = "game";
+  // let gameTickDiv = document.getElementById(gameTickId);
+  // if (!gameTickDiv) {
+  //   gameTickDiv = document.createElement("div");
+  //   gameTickDiv.id = gameTickId;
+  //   gameInfoDiv?.appendChild(gameTickDiv);
+  // }
+  // gameTickDiv.innerText = "Game tick: " + gameTick.toString();
+
+  // Ships Info
+  const shipsInfoDiv = getOrCreateElementById({id: "shipsInfo"});
+  ships.forEach((ship) => {
+    const shipInfoDiv = getOrCreateElementById({id: "shipInfo-" + ship.name});
+    shipInfoDiv.className = "shipInfo";
+
+    const shipName = getOrCreateElementById({id: "shipInfo-name-" + ship.name, innerText: ship.name});
+    const destination = getOrCreateElementById({id: "shipInfo-destination-" + ship.name, innerText: "Destination: Planet " + ship.destination2.name});
+    const speed = getOrCreateElementById({id: "shipInfo-speed-" + ship.name, innerText: "Speed: " + ship.speed});
+    const capacity = getOrCreateElementById({id: "shipInfo-capacity-" + ship.name, innerText: "Capacity: " + ship.capacity});
+
+    const addSpeedId = "addSpeed" + ship.name;
+    const addSpeedButton = getOrCreateButton({
+      id: addSpeedId,
+      textContent: "Upgrade speed " + "(" + ship.upgradeSpeedCost + " credits)",
+      onclick:  () => { upgradeShipSpeed(ship)},
+      disabled: credits < ship.upgradeSpeedCost || ship.speed === 4,
+    });
+    if (ship.speed === 4) {
+      speed.textContent += " (MAX)";
+      addSpeedButton.textContent = "Upgrade speed"
+      addSpeedButton.disabled = true;
+    }
+
+    const addCapacityId = "addCapacity" + ship.name;
+    const addCapacityButton = getOrCreateButton({
+      id: addCapacityId,
+      textContent: "Upgrade capacity " + "(" + ship.upgradeCapacityCost + " credits)",
+      onclick: () => { upgradeShipCapacity(ship) },
+      disabled: credits < ship.upgradeCapacityCost,
+    });
+    if (ship.capacity === 4) {
+      capacity.textContent += " (MAX)";
+      addCapacityButton.textContent = "Upgrade capacity"
+      addCapacityButton.disabled = true;
+    }
+
+    shipInfoDiv.appendChild(shipName);
+    shipInfoDiv.appendChild(destination);
+    shipInfoDiv.appendChild(speed);
+    shipInfoDiv.appendChild(capacity);
+    shipInfoDiv.appendChild(addSpeedButton);
+    shipInfoDiv.appendChild(addCapacityButton);
+    shipsInfoDiv.appendChild(shipInfoDiv);
+  });
+
+  // Add ship to existing planet
+  const planetsInfo = getOrCreateElementById({id: "planetsInfo"});
+  destinationPlanets.forEach((planet) => {
+    const planetId = "planetInfo-" + planet.name;
+    const planetInfo = getOrCreateElementById({id: planetId});
+    planetInfo.className = "planetInfo";
+
+    const planetName = getOrCreateElementById({id: planetId + "-name", innerText: "Planet " + planet.name});
+    const planetDisplay = getOrCreateElementById({id: planetId + "-display", innerText: planet.display});
+    planetDisplay.className = "planetDisplay";
+
+    const distance = getOrCreateElementById({id: planetId + "-distance", innerText: "Distance: " + planet.pos});
+    const goodsMultiplier = getOrCreateElementById({id: planetId + "-goods", innerText: "Goods multiplier: " + planet.goods});
+
+    const addShipButton = getOrCreateButton({
+      id: "addShip" + planet.name,
+      textContent: "Launch new ship (" + planet.launchCost + " credits)",
+      onclick: () => {
+        const newShip = addShipToPlanet(planet)
+        if (newShip) {
+          ships.push(newShip);
+        }
+      },
+      disabled: credits < planet.launchCost,
+    });
+    planetsInfo.append(planetInfo);
+    planetInfo.appendChild(planetName);
+    planetInfo.appendChild(planetDisplay);
+    planetInfo.appendChild(distance);
+    planetInfo.appendChild(goodsMultiplier);
+    planetInfo.appendChild(addShipButton);
+  });
+}
+
+main();
